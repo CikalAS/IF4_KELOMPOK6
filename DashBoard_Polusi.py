55378ab3141c5dc2598657457285755c683c2696
IF4_KELOMPOK6 <cikalagustian69@gmail.com>
2025-02-14 01:37:28 +0700

    Update DashBoard_Polusi.py

--- DashBoard_Polusi.py
+++ DashBoard_Polusi.py
@@ -66,7 +66,7 @@ st.markdown("""
         color: #2c3e50;
     }
     label, .stSelectbox div, .stTextInput div, .stNumberInput div {
-        color: black !important;
+        color: red !important;
     }
 </style>
 """, unsafe_allow_html=True)
